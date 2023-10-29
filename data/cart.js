5619fc1a6d39861f284668f5a35d9aa151c1fa05
shubham <shubhankarswain16@gemail.com>
2023-10-30 02:27:21 +0530

    fkjgkf

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,22 @@
-export let cart=[
-    {
-        productid: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-        quantity:2
-    },
-    {
-        productid: "dd82ca78-a18b-4e2a-9250-31e67412f98d",
-        quantity:1
-    }
-];
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if(!cart){
+  cart = [{
+            productid: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+            quantity:2
+        },
+        {
+            productid: "dd82ca78-a18b-4e2a-9250-31e67412f98d",
+            quantity:1
+        }
+    ];
+    
+}
+
+function store_the_cart(){
+    localStorage.setItem('cart',JSON.stringify(cart));
+}
+
 export function addtocart(product_id){
     
     
@@ -29,6 +38,8 @@ export function addtocart(product_id){
         });
         // console.log(cart);
     }
+    
+    store_the_cart();
 
 }
 
@@ -40,4 +51,5 @@ export function remove_to_cart(pro_id){
         }
     });
     cart=array;
+    store_the_cart();
 }
